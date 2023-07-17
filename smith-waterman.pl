a393fd3138b7b865df2752151711a11f19093cca
jananikangesan <janakanges1996@gmail.com>
2023-07-17 14:47:07 +0530

    changes

--- smith-waterman.pl
+++ smith-waterman.pl
@@ -1,8 +1,8 @@
 # Smith-Waterman  Algorithm
 
 #initialize the sequences
-my $seq1="ATGCT"; 
-my $seq2="AGCT";
+my $sequence1="ATGCT"; 
+my $sequence2="AGCT";
 
 # scoring scheme
 my $MATCH    =  1; # +1 for letters that match
@@ -15,11 +15,11 @@ my @matrix;
 $matrix[0][0]{score}   = 0;
 $matrix[0][0]{pointer} = "none";
 
-for(my $j = 1; $j <= length($seq1); $j++) {
+for(my $j = 1; $j <= length($sequence1); $j++) {
     $matrix[0][$j]{score}   = 0;
     $matrix[0][$j]{pointer} = "none";
 }
-for (my $i = 1; $i <= length($seq2); $i++) {
+for (my $i = 1; $i <= length($sequence2); $i++) {
     $matrix[$i][0]{score}   = 0;
     $matrix[$i][0]{pointer} = "none";
 }
@@ -29,13 +29,13 @@ my $max_i     = 0;
 my $max_j     = 0;
 my $max_score = 0;
 
-for(my $i = 1; $i <= length($seq2); $i++) {
-    for(my $j = 1; $j <= length($seq1); $j++) {
+for(my $i = 1; $i <= length($sequence2); $i++) {
+    for(my $j = 1; $j <= length($sequence1); $j++) {
         my ($diagonal_score, $left_score, $up_score);
         
         # calculate match score
-        my $letter1 = substr($seq1, $j-1, 1);
-        my $letter2 = substr($seq2, $i-1, 1);       
+        my $letter1 = substr($sequence1, $j-1, 1);
+        my $letter2 = substr($sequence2, $i-1, 1);       
         if ($letter1 eq $letter2) {
             $diagonal_score = $matrix[$i-1][$j-1]{score} + $MATCH;
         }
@@ -83,9 +83,21 @@ for(my $i = 1; $i <= length($seq2); $i++) {
     }
 }
 
-for(my $i=0;$i<length($seq2)+1;$i++){
-	for(my $j=0;$j<length($seq1)+1;$j++){
+#printing the matrix score
+
+for(my $i=0;$i<length($sequence2)+1;$i++){
+	for(my $j=0;$j<length($sequence1)+1;$j++){
 		print ("  ",$matrix[$i][$j]{score},"   ");
+	}
+	print("\n");
+}
+print("\n\n");
+
+#printing the matrix pointer
+
+for(my $i=0;$i<length($sequence2)+1;$i++){
+	for(my $j=0;$j<length($sequence1)+1;$j++){
+		print ("  ",$matrix[$i][$j]{pointer},"   ");
 	}
 	print("\n");
 }
@@ -97,31 +109,28 @@ print("\n\n");
 my $align1 = "";
 my $align2 = "";
 
-my $j = $max_j;
-my $i = $max_i;
+my $j = $max_j; #column
+my $i = $max_i; #row
 
-while (1) {
-    last if $matrix[$i][$j]{pointer} eq "none";
+while ($matrix[$i][$j]{pointer} ne "none") {
     
     if ($matrix[$i][$j]{pointer} eq "diagonal") {
-        $align1 .= substr($seq1, $j-1, 1);
-        $align2 .= substr($seq2, $i-1, 1);
+        $align1 = substr($sequence1, $j-1, 1). $align1 ;
+        $align2 = substr($sequence2, $i-1, 1).$align2 ;
         $i--; $j--;
     }
     elsif ($matrix[$i][$j]{pointer} eq "left") {
-        $align1 .= substr($seq1, $j-1, 1);
-        $align2 .= "-";
+        $align1 = substr($sequence1, $j-1, 1).$align1 ;
+        $align2 = "-".$align2 ;
         $j--;
     }
     elsif ($matrix[$i][$j]{pointer} eq "up") {
-        $align1 .= "-";
-        $align2 .= substr($seq2, $i-1, 1);
+        $align1 = "-". $align1 ;
+        $align2 = substr($sequence2, $i-1, 1).$align2 ;
         $i--;
     }   
 }
 
-$align1 = reverse $align1;
-$align2 = reverse $align2;
 
 print "$align1\n";
 print "$align2\n";
